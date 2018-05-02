316fac4ee8dd7396ad28e5c00219f654fc0a7ef1
Molly Wood <mollywood@Interzone.localdomain>
2018-05-02 12:23:51 -0500

    updating

--- calculator.py
+++ calculator.py
@@ -1,20 +1,20 @@
+def calc():
+    # +
+    if (oper == "add"):
+        result = num_1 + num_2
+        print(f"The sum of {num_1} and {num_2} is {result}.")
 
-# +
-if (oper == "add"):
-    result = num_1 + num_2
-    print(f"The sum of {num_1} and {num_2} is {result}.")
+    # -
+    elif oper == "subtract":
+        result = num_1 - num_2
+        print(f"{num_1} minus {num_2} is {result}.")
 
-# -
-elif oper == "subtract":
-    result = num_1 - num_2
-    print(f"{num_1} minus {num_2} is {result}.")
+    # *
+    elif oper == "multiply":
+        result = num_1 * num_2
+        print(f"{num_1} times {num_2} is {result}.")
 
-# *
-elif oper == "multiply":
-    result = num_1 * num_2
-    print(f"{num_1} times {num_2} is {result}.")
-
-# /
-elif oper == "divide":
-    result = num_1 / num_2
-    print(f"{num_1} divided by {num_2} is {result}.")
+    # /
+    elif oper == "divide":
+        result = num_1 / num_2
+        print(f"{num_1} divided by {num_2} is {result}.")
--- using_calc.py
+++ using_calc.py
@@ -1,7 +1,11 @@
 # building a calculator with Python
 
+import calculator
+
 # taking an input from the user
 
 num_1 = int(input("Please input your first number: "))
 num_2 = int(input("Please input your second number: "))
 oper = input("Finally, name the operation that you would like to perform (add, subtract, multiply, divide): ")
+
+calc()
